801e79f08daca690cac28d0bcb6f856b8e118297
rudro1012 <samiulislamrudro75754@gmail.com>
2025-08-08 02:23:13 +0600

    tranformation matrix of frame 4 relative to frame 0

--- my_manipulator/kinematic_solver.py
+++ my_manipulator/kinematic_solver.py
@@ -9,6 +9,7 @@ theta2=symbols('theta2')
 theta3=symbols('theta3')
 
 
+# dh parameter measured form the physical system
 dh_table=[
     {},
     {'alpha': 0,     'a': 0,    'd': 9.2, 'theta':theta1},
@@ -17,10 +18,41 @@ dh_table=[
     {'alpha': 0,     'a': 13.4, 'd': 0,   'theta':0},
 ]
 
-
+# defining general transformation matrix for each frame 
 def matrix(i) :
-    transformation_matrix=1
+    
+    alpha=dh_table[i]['alpha']
+    a=dh_table[i]['a']
+    d=dh_table[i]['d']
+    theta=dh_table[i]['theta']
+
+    # from john j craig, intro to robotics, 3rd edition, chapter 3, section 3.5
+    # t(i-1)i(transformation matrix of frame i relative to frame i-1)=Rx[alpha(i-1]*Dx[a(i-1)]*Rz(thetai)*Dz(di)
+
+    transformation_matrix=Matrix([[ cos(theta)            ,      -sin(theta)      ,      0      ,         a    ],
+                                  [ sin(theta)*cos(alpha) , cos(theta)*cos(alpha) , -sin(alpha) , -sin(alpha)*d],
+                                  [ sin(theta)*sin(alpha) , cos(theta)*sin(alpha) ,  cos(alpha) ,  cos(alpha)*d],
+                                  [           0           ,           0           ,       0     ,         1    ]
+                                 ])
+
+
     return transformation_matrix 
+
+# tmn= transformation matrix of n relative to m
+
+t01=matrix(1)
+t12=matrix(2)
+t23=matrix(3)
+t34=matrix(4)
+
+
+t04=t01*t12*t23*t34
+
+print(t04)
+# x=dh_table[1]['d']
+# y=x*3
+# print(y)
+
 
 
 # x=dh_table[3]['alpha2']
