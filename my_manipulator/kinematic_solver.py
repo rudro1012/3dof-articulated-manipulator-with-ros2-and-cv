e7034e23c37006f9191c5288cd5b414ec2235ea2
rudro1012 <samiulislamrudro75754@gmail.com>
2025-08-08 00:55:43 +0600

    added matrix function

--- my_manipulator/kinematic_solver.py
+++ my_manipulator/kinematic_solver.py
@@ -2,18 +2,26 @@ from sympy import *
 import numpy as np
 import math
 
+init_printing()
+
 theta1=symbols('theta1')
 theta2=symbols('theta2')
 theta3=symbols('theta3')
-theta4=symbols('theta4')
+
 
 dh_table=[
     {},
-    {'alpha0': 0,     'a0': 0,    'd1': 9.2, 'theta1':theta1},
-    {'alpha1': -pi/2, 'a1': 1.1,  'd2': 0,   'theta2':theta2},
-    {'alpha2': pi,    'a2': 10.4, 'd3': 0,   'theta3':theta1},
-    {'alpha3': 0,     'a3': 13.4, 'd4': 0,   'theta4':theta1},
+    {'alpha': 0,     'a': 0,    'd': 9.2, 'theta':theta1},
+    {'alpha': -pi/2, 'a': 1.1,  'd': 0,   'theta':theta2},
+    {'alpha': pi,    'a': 10.4, 'd': 0,   'theta':theta3},
+    {'alpha': 0,     'a': 13.4, 'd': 0,   'theta':0},
 ]
 
-x=dh_table[3]['alpha2']
-print(sec(x))
+
+def matrix(i) :
+    transformation_matrix=1
+    return transformation_matrix 
+
+
+# x=dh_table[3]['alpha2']
+# print(sec(x))
