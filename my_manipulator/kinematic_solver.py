554072e5c5f6e58ab60b264ca4f758a8684d3289
rudro1012 <samiulislamrudro75754@gmail.com>
2025-08-16 03:53:48 +0600

    ik solved

--- my_manipulator/kinematic_solver.py
+++ my_manipulator/kinematic_solver.py
@@ -1,89 +1,43 @@
-from sympy import *
-# import numpy as np
-import math
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+import numpy as np
 
-init_printing()
+my_chain = Chain(name='my_robot', links=[
+    OriginLink(),
+    URDFLink(
+        name="joint1",
+        origin_translation=[0, 0, 0],  # Translation vector
+        origin_orientation=[0, 0, 0],  # Orientation (roll, pitch, yaw)
+        rotation=[0, 0, 1],           # Rotation axis (x, y, z)
+        bounds=(0, np.pi)             # Joint limits (0 to 180 degrees)
+    ),
+    URDFLink(
+    name="joint2",
+    origin_translation=[1.1,0,0],
+    origin_orientation=[-90*np.pi/180,0,0],  # alpha=-pi/2
+    rotation=[0,0,1],  # rotate around z
+    bounds=(0,np.pi)
 
-θ1=symbols('θ1')
-θ2=symbols('θ2')
-θ3=symbols('θ3')
-
-
-# dh parameter measured form the physical system
-dh_table=[
-    {},
-    {'alpha': 0,     'a': 0,    'd': 0,   'θ':θ1},
-    {'alpha': -pi/2, 'a': 1.1,  'd': 0,   'θ':θ2},
-    {'alpha': pi,    'a': 10.4, 'd': 0,   'θ':θ3},
-    {'alpha': 0,     'a': 13.4, 'd': 0,   'θ':0},
-]
-
-tfik=Matrix([[1,0,0,11],
-             [0,1,0,20],
-             [0,0,1,3],
-             [0,0,0,1]
-             ])
-
-new_x=0
-if (tfik[0,3]<0):
-    new_x=tfik[0,3]
-    tfik[0,3]=(-1)*tfik[0,3]
-
-print(tfik[0,3])
-# defining general transformation matrix for each frame 
-def matrix(i) :
-    
-    alpha=dh_table[i]['alpha']
-    a=dh_table[i]['a']
-    d=dh_table[i]['d']
-    θ=dh_table[i]['θ']
-
-    # from john j craig, intro to robotics, 3rd edition, chapter 3, section 3.5
-    # t(i-1)i(transformation matrix of frame i relative to frame i-1)=Rx[alpha(i-1]*Dx[a(i-1)]*Rz(θi)*Dz(di)
-
-    transformation_matrix=Matrix([[ cos(θ)            ,      -sin(θ)      ,      0      ,         a    ],
-                                  [ sin(θ)*cos(alpha) , cos(θ)*cos(alpha) , -sin(alpha) , -sin(alpha)*d],
-                                  [ sin(θ)*sin(alpha) , cos(θ)*sin(alpha) ,  cos(alpha) ,  cos(alpha)*d],
-                                  [       0           ,       0           ,       0     ,         1    ]
-                                 ])
+    ),
+    URDFLink(
+        name="joint3",
+        origin_translation=[10.4, 0, 0],
+        origin_orientation=[np.pi, 0, 0],
+        rotation=[0, 0, 1],
+        bounds=(0, np.pi)
+    ),
+    URDFLink(
+        name="end_effector",
+        origin_translation=[13.4, 0, 0],
+        origin_orientation=[0, 0, 0],
+        rotation=[0, 0, 0]  # Fixed joint
+    )
+])
 
 
-    return transformation_matrix 
+tfik = [10,18,3]
 
-# tmn= transformation matrix of n relative to m
+angles_rad = my_chain.inverse_kinematics(tfik)
+angles_deg = [angle * 180 / np.pi for angle in angles_rad]
 
-t01=matrix(1)
-t12=matrix(2)
-t23=matrix(3)
-t34=matrix(4)
-
-
-t04=t01*t12*t23*t34
-
-
-
-
-
-# print(t04[0,3])
-# print(t04[1,3])
-# print(t04[2,3])
-eq1=Eq(t04[0,3],tfik[0,3])
-print(eq1)
-eq2=Eq(t04[1,3],tfik[1,3])
-eq3=Eq(t04[2,3],tfik[2,3])
-
-solution=nsolve([eq1,eq2,eq3],(θ1,θ2,θ3),(1.57,1.57,1.57))
-
-if(new_x<0):
-    solution[0,0]=3.1415-solution[0,0]
-
-print(solution)
-
-# x=dh_table[1]['d']
-# y=x*3
-# print(y)
-
-
-
-# x=dh_table[3]['alpha2']
-# print(sec(x))
+print("Joint angles (deg):", angles_deg)
