0c88d8b805c19d5b3ff28067537c4da5bb4a47f6
rudro1012 <samiulislamrudro75754@gmail.com>
2025-08-21 23:29:41 +0600

    function declaration

--- my_manipulator/kinematic_solver.py
+++ my_manipulator/kinematic_solver.py
@@ -1,43 +1,21 @@
 from ikpy.chain import Chain
-from ikpy.link import OriginLink, URDFLink
+from ikpy.link import OriginLink,URDFLink
 import numpy as np
-
-my_chain = Chain(name='my_robot', links=[
-    OriginLink(),
-    URDFLink(
-        name="joint1",
-        origin_translation=[0, 0, 0],  # Translation vector
-        origin_orientation=[0, 0, 0],  # Orientation (roll, pitch, yaw)
-        rotation=[0, 0, 1],           # Rotation axis (x, y, z)
-        bounds=(0, np.pi)             # Joint limits (0 to 180 degrees)
-    ),
-    URDFLink(
-    name="joint2",
-    origin_translation=[1.1,0,0],
-    origin_orientation=[-90*np.pi/180,0,0],  # alpha=-pi/2
-    rotation=[0,0,1],  # rotate around z
-    bounds=(0,np.pi)
-
-    ),
-    URDFLink(
-        name="joint3",
-        origin_translation=[10.4, 0, 0],
-        origin_orientation=[np.pi, 0, 0],
-        rotation=[0, 0, 1],
-        bounds=(0, np.pi)
-    ),
-    URDFLink(
-        name="end_effector",
-        origin_translation=[13.4, 0, 0],
-        origin_orientation=[0, 0, 0],
-        rotation=[0, 0, 0]  # Fixed joint
+def joint_description(joint_name:str,alpha:float,a:float,d:float,theta:float,lower_limit:float,higher_limit:float):
+    if not isinstance(joint_name,str):
+        raise TypeError('Joint name has to be an string followed by a number')
+    for var_value in [alpha,a,d,theta,lower_limit,higher_limit]:
+        if not isinstance(var_value,float):
+            raise TypeError(f'{var_value} have to be a float')
+    
+    joint_property=URDFLink(
+        name=joint_name,
+        origin_translation=[a,0,d],
+        origin_orientation=[alpha,0,theta],
+        rotation=[0,0,1],
+        bounds=[]
     )
-])
 
 
-tfik = [10,18,3]
 
-angles_rad = my_chain.inverse_kinematics(tfik)
-angles_deg = [angle * 180 / np.pi for angle in angles_rad]
-
-print("Joint angles (deg):", angles_deg)
+x=joint_description('my',1.00,4.00,0,0.0,np.pi)
